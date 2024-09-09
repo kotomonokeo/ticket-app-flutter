39b6a3696647d4cea69dfa84ea41c35c15016cea
kotomonokeo <kotomonokeo@gmail.com>
2024-09-09 12:28:50 +0700

    add separate file for variable

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../base/res/styles/app_styles.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -14,9 +16,40 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: ListView(
         children: [
-          Container(child: Text('hallo home screen'),),
-          Container(child: Text('hellow jug'),)
-          
+          Container(
+            color: Colors.blue,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Good morning!',
+                            style: AppStyles.headLineStyle3),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text('Book Ticket',
+                            style: AppStyles.headLineStyle1)
+                      ],
+                    ),
+                    Container(
+                      color: Colors.red,
+                      width: 100,
+                      height: 70,
+                    )
+                  ],
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text('Search Icon'), Text('Empty Space')],
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
